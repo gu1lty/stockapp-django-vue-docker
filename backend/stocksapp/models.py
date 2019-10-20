f732249cb101bfda587a50f1a14d36ce390dd8e4
Rax Otero <oterorax@gmail.com>
2019-10-20 09:35:08 +0800

    Created models and updated dockerfile

--- backend/stocksapp/models.py
+++ backend/stocksapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    sku = models.CharField(max_length=50)
+    quatity = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+class Transaction(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=0)
+    transactionType = models.IntegerField()
